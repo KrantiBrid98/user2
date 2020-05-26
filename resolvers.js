5e639be8434b500abb3ff7e520eafd0448ef972a
KrantiBrid98 <45895279+KrantiBrid98@users.noreply.github.com>
2020-05-26 09:20:11 +0530

    Add files via upload
    initial commit

---
+++ resolvers.js
@@ -0,0 +1,75 @@
+const axios = require('axios')
+
+module.exports = {
+
+  // Query resolvers
+  Query: {
+    users: (parentValue, args) => {
+      console.log(parentValue, 'paremtva')
+      return axios.get(`http://localhost:3000/users`)
+        .then(res => res.data)
+    },
+
+    company: (parentValue, args) => {
+      return axios.get(`http://localhost:3000/companies/${args.id}`)
+        .then(res => res.data)
+    },
+
+    companies: (parentValue, args) => {
+      console.log(parentValue, args, 'paremtva')
+      return axios.get(`http://localhost:3000/companies`)
+        .then(res => res.data)
+    }
+  },
+
+  // Custom resolver types
+  User: {
+    // go to user and find any relation to resolve. 
+    // Found company relation to resolve so added that
+    company(parentValue, args) {
+      return axios.get(`http://localhost:3000/companies/${parentValue.companyId}`)
+        .then(res => res.data)
+    }
+  },
+
+  Company: {
+    user(parentValue, args) {
+      return axios.get(`http://localhost:3000/company/${parentValue.id}/users`)
+        .then(res => res.data)
+    }
+  },
+
+  Mutation: {
+    addUser(parentValue, args) {
+      return axios.post('http://localhost:3000/users/', {
+        firstName: args.firstName,
+        age: args.age
+      })
+        .then(res => res.data)
+    },
+
+    editUser(parentValue, args) {
+      console.log(args.id, 'patch')
+      return axios.patch(`https://jsonplaceholder.typicode.com/users/${args.id}`, args)
+        .then(res => res.data)
+    },
+
+    deleteUser(parentValue, args) {
+      return axios.delete(`https://localhost:3000/users/${args.id}`)
+        .then(res => res.data)
+    }
+  }
+}
+
+
+// query {
+//   users {
+//     id
+//     firstName,
+//     company{
+//       id,
+//       name,
+//       description
+//     }
+//   }
+// }
---
+++ schema.js
@@ -0,0 +1,45 @@
+const { gql } = require('apollo-server')
+
+const typeDefs = gql`   
+    type Company {
+        id: Int
+        name: String
+        description: String
+        user: [User]
+    }
+
+    type User {
+        id: String
+        firstName: String
+        age: Int
+        companyId: Int
+        company: Company
+    }
+
+    type Query {
+        users: [User]
+        company(id: Int): Company
+        companies: [Company]
+    }
+
+    type Mutation {
+        addUser(firstName: String, age: Int): User
+        deleteUser(id: String): User
+        editUser(firstName: String, age: Int, id: String, companyId: Int): User
+    }
+
+`
+
+// query {
+//     users {
+//       id
+//       firstName,
+//       company{
+//         id,
+//         name,
+//         description
+//       }
+//     }
+//   }
+
+module.exports = typeDefs
